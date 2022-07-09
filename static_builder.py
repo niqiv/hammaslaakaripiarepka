3c35727e21652954da77493662902f60d0c43fa8
Niki Vihtola <niki@vihtola.fi>
2022-07-09 12:18:23 +0300

    added some backend and seo changes

--- static_builder.py
+++ static_builder.py
@@ -2,9 +2,11 @@ import os
 import sys
 import jinja2
 from distutils.dir_util import copy_tree
+import shutil
 
 def main():
 
+	# Init paths, root path, and development to app
 	root_abs_path = os.path.abspath(os.getcwd())
 	
 	SRC = os.path.join(root_abs_path, 'src')
@@ -23,15 +25,19 @@ def main():
 		return ''
 
 
-	# Clean App
+	# Clean app to build new by removing all contents of both app and app/static
+	# Preserve directory structure
 	for f in os.listdir(APP):
 		if os.path.isfile(os.path.join(APP, f)):
 			os.remove(os.path.join(APP, f))
 	for f in os.listdir(APP_STATIC):
 		os.remove(os.path.join(APP_STATIC, f))
 
+	# Copy all static elements from src to app
+	# TODO: only used elements
 	copy_tree(SRC_STATIC, APP_STATIC)
 
+	# Find all templates from source
 	tpls = [i for i in os.listdir(SRC) if i.endswith('.html.tpl')]
 
 	if '--local' in sys.argv[1:]:
@@ -39,6 +45,7 @@ def main():
 	else:
 		build_ext = ''
 
+	# Build templates
 	for i in tpls:
 		template = templateenv.get_template(i)
 		if i == 'index.html.tpl':
@@ -48,6 +55,16 @@ def main():
 		with open(os.path.join(APP, filename), 'w') as f:
 			f.write(template.render()) 
 
+	def add_static_file(path):
+		if os.path.isfile(os.path.join(SRC, path)):
+			shutil.copyfile(
+				os.path.join(SRC, path),
+				os.path.join(APP, path)
+			)
+
+	# Add robots.txt and sitemap.xml
+	add_static_file('robots.txt')
+	add_static_file('sitemap.xml')
 
 	return
 
