e3bd5fe241fa6dc3f2ff1919ca6a36cac47b6bfc
Niki Vihtola <niki@vihtola.fi>
2023-10-30 21:35:07 +0200

    hinnasto changes

--- static_builder.py
+++ static_builder.py
@@ -1,72 +1,77 @@
 import os
 import sys
 import jinja2
-from distutils.dir_util import copy_tree
 import shutil
 
-def main():
-
-	# Init paths, root path, and development to app
-	root_abs_path = os.path.abspath(os.path.dirname(__file__))
-	
-	SRC = os.path.join(root_abs_path, 'src')
-	SRC_STATIC = os.path.join(SRC, 'static')
-	
-	APP = os.path.join(root_abs_path, 'app')
-	APP_STATIC = os.path.join(APP, 'static')
-
-	templateloader = jinja2.FileSystemLoader(searchpath=SRC)
-	templateenv = jinja2.Environment(loader=templateloader)
-
-	def include(path):
-		return ''
-
-	def static(path):
-		return ''
+USAGE_TEXT = '''Usage:
+build: Compile content to pages
+run:   Build local files and run webserver
+help:  Show this text'''
 
 
-	# Clean app to build new by removing all contents of both app and app/static
-	# Preserve directory structure
-	for f in os.listdir(APP):
-		if os.path.isfile(os.path.join(APP, f)):
-			os.remove(os.path.join(APP, f))
-	for f in os.listdir(APP_STATIC):
-		os.remove(os.path.join(APP_STATIC, f))
+def build():
 
-	# Copy all static elements from src to app
-	# TODO: only used elements
-	copy_tree(SRC_STATIC, APP_STATIC)
+    # Init paths, root path, and development to app
+    root_abs_path = os.path.abspath(os.path.dirname(__file__))
 
-	# Find all templates from source
-	tpls = [i for i in os.listdir(SRC) if i.endswith('.html.tpl')]
+    src = os.path.join(root_abs_path, 'src')
+    src_static = os.path.join(src, 'static')
 
-	if '--local' in sys.argv[1:]:
-		build_ext = '.html'
-	else:
-		build_ext = ''
+    app = os.path.join(root_abs_path, 'app')
+    app_static = os.path.join(app, 'static')
 
-	# Build templates
-	for i in tpls:
-		template = templateenv.get_template(i)
-		if i == 'index.html.tpl':
-			filename = 'index.html'
-		else:
-			filename = i.replace('.html.tpl', build_ext)
-		with open(os.path.join(APP, filename), 'w') as f:
-			f.write(template.render()) 
+    templateloader = jinja2.FileSystemLoader(searchpath=src)
+    templateenv = jinja2.Environment(loader=templateloader)
 
-	def add_static_file(path):
-		if os.path.isfile(os.path.join(SRC, path)):
-			shutil.copyfile(
-				os.path.join(SRC, path),
-				os.path.join(APP, path)
-			)
+    # Clean app to build new by removing all contents of both app and app/static
+    # Preserve directory structure
+    shutil.rmtree(app, ignore_errors=True)
+    os.makedirs(app, exist_ok=True)
 
-	# Add robots.txt and sitemap.xml
-	add_static_file('robots.txt')
-	add_static_file('sitemap.xml')
+    # Copy all static elements from src to app
+    # TODO: only used elements
+    shutil.copytree(src_static, app_static)
 
-	return
+    # Find all templates from source
+    tpls = [i for i in os.listdir(src) if i.endswith('.html.tpl')]
+
+    # Build templates
+    for i in tpls:
+        template = templateenv.get_template(i)
+        if i == 'index.html.tpl':
+            filename = 'index.html'
+        else:
+            filename = i.replace('.html.tpl', '')
+        with open(os.path.join(app, filename), 'w') as f:
+            f.write(template.render())
+
+    def add_static_file(path):
+        if os.path.isfile(os.path.join(src, path)):
+            shutil.copyfile(
+                os.path.join(src, path),
+                os.path.join(app, path)
+            )
+
+    # Add robots.txt and sitemap.xml
+    add_static_file('robots.txt')
+    add_static_file('sitemap.xml')
+
+    return
+
+
+def run():
+    # TODO: Locally running server like npm watch
+    pass
+
 
 if __name__ == '__main__':
-	main()
+    mode = sys.argv[1]
+
+    if mode == 'help' or mode not in ('build', 'run', 'help'):
+        print(USAGE_TEXT)
+
+    else:
+        build()
+
+    if mode == 'run':
+        run()
